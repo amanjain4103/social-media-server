e2d0fbdd5ed1eb350c3d8e90eb086ab1a1d54c86
Aman Jain <amanjain4103@gmail.com>
2020-10-05 09:24:29 +0530

    removed credentials

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    serviceID: "VAf586eddad0af8822601c1ae65d1d2722",
-    accountSID: "AC1911cebe509cc855da7340b7e576bd71",
-    authToken: "2d94bdf12a4191f0dcaccf090d7aa30f"
+    serviceID: "serviceid_from_twilio",
+    accountSID: "accountsid_from_twilio",
+    authToken: "authtoken_from_twilio"
 }
